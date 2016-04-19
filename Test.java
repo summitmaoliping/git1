635b76bf4629bd8fb018cd7692ab05ecff255d09
summmitmaoliping <577561091@qq.com>
2016-04-19 22:57:56 +0800

    初始化提交

---
+++ Test.java
@@ -0,0 +1,21 @@
+package com.briup.ch10;
+    public class Test
+    {
+
+    public static void main(String[] args){
+		Test test=new Test();
+		test.test1();
+		}
+		public void test1()
+	
+	{
+		int[]arr=new int[]{5,2,3,7,8,9};
+		System.out.println("数组长度为："+arr.length);
+		System.out.println(arr);
+		for(int i=0;i<arr.length;i++)
+			{
+
+         System.out.println(arr[i]);
+		}
+	}
+	}
